f0e92e113ae96a9ad7e73b5fdab6bf26af3b5353
Niu Tieliang <58173551+shawnw19@users.noreply.github.com>
2020-04-29 18:55:04 +0800

    Add files via upload

--- Signals/$5_OtherTestSounds.java
+++ Signals/$5_OtherTestSounds.java
@@ -5,7 +5,7 @@ import IntroCS_Princeton.StdAudio;
 
 /*
 synthesis of sawtooth, triangle and impulse sounds
-based on [Hack audio  an introduction to computer programming and digital signal processing in MATLAB]
+based on [Hack audio an introduction to computer programming and digital signal processing in MATLAB]
 chp7.3.4-7.3.6
  */
 public class $5_OtherTestSounds {
@@ -13,13 +13,11 @@ public class $5_OtherTestSounds {
     static int Fs = StdAudio.SAMPLE_RATE;
     static int length = (int) (duration*Fs);
     static int f1 = 880;//sawtooth
-    static int f2 = 880;//triangle
+    static int f2 = 480;//triangle
     static int f3 = 440;//impulse
-    //static int[] f =new int[]{f1,f2,f3};
     static int period;// = new int[3] int[]of three frequencies, duration*Fs/f[i]
-    //static double[] sound;
     public static void main(String[] args) {
-        double min = -1, max = 1;
+        //double min = -1, max = 1;
 
         double[] sound = generateSound(2);
 
@@ -33,17 +31,17 @@ public class $5_OtherTestSounds {
     static double[] generateSound(int theCase){
         double[] sound = new double[length];
         if (theCase==1){
-            period = length/f1;
+            period = Fs/f1;
             for (int i = 0; i <length ; i++) {
-                sound[i] = (1.8/period)*(i%period) -1;//a line
+                sound[i] = (1.8/period)*(i%period) -1;//a line, 1.8 is per orig. book
             }
         }else if(theCase==2){
-            period = length/f2;
+            period = Fs/f2;
             for (int i = 0; i <length ; i++) {
                 sound[i] =1- (4.0/period)*Math.abs(i%period -0.5*period);//opposite lines against the peak
             }
         }else if(theCase==3){
-            period = length/f3;
+            period = Fs/f3;
             for (int i = 0; i <length ; i++) {
                 sound[i] = (i%period == 0)?1:0;//only pulses
             }
@@ -51,4 +49,5 @@ public class $5_OtherTestSounds {
 
         return sound;
     }
+
 }
---
+++ Signals/$6_TremoloEtAh.java
@@ -0,0 +1,73 @@
+package Signals;
+
+import CommonUtils.SignalUtilis;
+import IntroCS_Princeton.StdAudio;
+
+/*
+the essence of this ubiquitous sound effect is
+the convolution of a low frequency oscillator(LFO) with another sound
+ */
+public class $6_TremoloEtAh {
+    static double duration = 5.0;
+    static int Fs = StdAudio.SAMPLE_RATE;
+    static int length = (int) (duration * Fs);
+    static int period;
+
+    static class SoundGen {//extracted and modified from $5
+        static double f1 = 4;//sawtooth
+        static int f2 = 880;//triangle
+        static int f3 = 440;//impulse
+
+        static double[] generateSound(int theCase) {
+            double[] sound = new double[length];
+            if (theCase == 1) {
+                period = (int) (Fs / f1);
+                for (int i = 0; i < length; i++) {
+                    sound[i] = (2.0 / period) * (i % period) - 1;//a line
+                }
+            } else if (theCase == 2) {
+                period = Fs / f2;
+                for (int i = 0; i < length; i++) {
+                    sound[i] = 1 - (4.0 / period) * Math.abs(i % period - 0.5 * period);//opposite lines against the peak
+                }
+            } else if (theCase == 3) {
+                period = Fs / f3;
+                for (int i = 0; i < length; i++) {
+                    sound[i] = (i % period == 0) ? 1 : 0;//only pulses
+                }
+            }
+
+            return sound;
+        }
+    }
+
+    public static void main(String[] args) {
+        double[] sound1 = SoundGen.generateSound(1);//sawtooth
+        double[] sound2 = SignalUtilis.generateSimpleSinusoid(duration, 800, 0);
+
+        double[] tremolo= SignalUtilis.simpleConvolution(sound1, sound2, 0.5);//plus offset which changes modulation depth/intensify by alternating the occurrence of peaks
+
+        //visualise a segment of new synthesised tremolo sound
+        double[] sample = SignalUtilis.smallSample(tremolo, 0.05);
+        SignalUtilis.visualizeSignal(sample,1700,550,5,true);
+
+        StdAudio.play(tremolo);
+
+        //try synthesising a vowel-like double-frequency-d sound
+        double[] sound3 = SignalUtilis.generateSimpleSinusoid(3.0, 200, 0);
+        double[] sound4 = SignalUtilis.generateSimpleSinusoid(3.0, 1000, 0);
+
+        double[] ahLike = SignalUtilis.simpleConvolution(sound3,sound4,0);
+
+        ahLike = SignalUtilis.linearFade(ahLike,0,0.05,true);
+        ahLike = SignalUtilis.linearFade(ahLike,0.95,1,false);
+
+        sample = SignalUtilis.smallSample(ahLike, 0.05);
+        SignalUtilis.visualizeSignal(sample,1700,550,1,true);
+
+        StdAudio.play(ahLike);
+
+        String file  = "/home/shawn/IdeaProjects/Streib/src/Signals/SignalFiles/$6_ahLike.wav";
+        StdAudio.save(file,ahLike);
+    }
+}
